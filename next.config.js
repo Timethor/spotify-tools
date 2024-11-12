198cc845cfad39114d1198fac1b92272440def7f
Nicholas Rich <nick.o.rich@gmail.com>
2024-11-11 22:52:59 -0500

    feat: carbon design system integration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    implementation: 'sass-embedded',
+    silenceDeprecations: ["legacy-js-api"],
+  },
+}
 
 module.exports = nextConfig
